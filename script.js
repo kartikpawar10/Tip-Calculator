3be4c8c72bdcf9fa4ebe364940b673ad1cc73738
Kartik Pawar <126799229+kartikpawar10@users.noreply.github.com>
2023-09-09 20:29:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const billTotalInputDiv = document.getElementById('billTotalInput')
+const tipInputDiv = document.getElementById('tipInput')
+const numberOfPeopleDiv= document.getElementById('numberOfPeople')
+const perPersonTotalDiv= document.getElementById('perPersonTotal')
+
+let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+
+const calculateBill = () => {
+    const bill =  Number(billTotalInputDiv.value)
+    const tipPercent = Number(tipInputDiv.value)/100
+    const tipAmount = bill*tipPercent
+    const total = tipAmount + bill
+    const perPersonTotal = total/numberOfPeople  
+    perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
+  }
+  const increasePeople = () => {
+    numberOfPeople+=1
+    numberOfPeopleDiv.innerText = numberOfPeople
+    calculateBill()
+  }
+  const decreasePeople = () => {
+    if(numberOfPeople<=1){
+      return
+    }
+    numberOfPeople-=1
+    numberOfPeopleDiv.innerText = numberOfPeople
+    calculateBill()
+  }
